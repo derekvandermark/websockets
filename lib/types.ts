42de73e0fb18f64a2893f3e8ccf9637f75cc0317
Derek van der Mark <derek.vandermark@gmail.com>
2024-06-10 21:26:06 -0400

    Update types

--- lib/types.ts
+++ lib/types.ts
@@ -1,1 +1,10 @@
+import { TLSSocket } from "tls";
+
 export type ReadyState = 'CONNECTING' | 'OPEN' | 'CLOSING' | 'CLOSED';
+
+export type Credentials = {
+    cert: Buffer,
+    key: Buffer
+}
+
+export type ConnectionListener = (ws: TLSSocket) => void;
