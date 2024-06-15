abd97e1dd5600bc2edd042534d30b159eb13ecf2
Derek van der Mark <derek.vandermark@gmail.com>
2024-06-14 23:28:12 -0400

    Begin upgrade request validation

--- lib/websocket-server.ts
+++ lib/websocket-server.ts
@@ -2,10 +2,11 @@ import EventEmitter from "events";
 import https from 'https';
 import http from 'http';
 import WebSocket from "./websocket";
-import { ConnectionListener, Credentials, Pathname } from "./types";
+import { ConnectionListener, Credentials, Pathname, WSSOptions } from "./types";
 import { TLSSocket, Server } from "tls";
 import { IncomingMessage } from "http";
 import { Duplex } from "stream";
+import { formatHttpResponse } from "./util";
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -15,56 +16,105 @@ export default class WebSocketServer extends EventEmitter {
     server: Server;
     #serviceRoute: Pathname;
     #wildcardRoute: boolean;
+    #origins: string[];
+    #requireOrigin: boolean;
     #subProtocol: string;
     //activeConnections
 
-    constructor(server: Server, route: Pathname, subProtocol?: string) { 
+    constructor(server: Server, route: Pathname, options?: WSSOptions) { 
         super();
         
         this.server = server;
         this.#serviceRoute = route;
         // this.#wildcardRoute = this.isWildcard(route);
-        this.#subProtocol = subProtocol;
+        this.#origins = options.origins;
+        this.#requireOrigin = options.requireOrigin;
+        this.#subProtocol = options.subProtocol;
 
         // Start server and attach main event listeners
         this.server.listen(port, hostname, () => {
-            console.log("WebSocket server running...");
+            console.log(`WebSocket server running at https://${hostname}:${port}`);
         });
 
         this.server.on('upgrade', (req, socket, head) => { 
+            console.log("in upgrade");
             this.handleUpgrade(req, socket, head);
         });
 
         this.server.on('request', (req, res) => {
+            console.log("in req");
             this.emit('request', req, res);
         });
     }
 
-    handleUpgrade(req: IncomingMessage, socket: Duplex, head: Buffer): void {
-  
+    handleUpgrade(req: IncomingMessage, socket: TLSSocket, head: Buffer): void {
+        if (this.routeInvalid(req)) {
+            this.abort(404, socket);
+        } else if (this.originInvalid(req)) {
+            this.abort(403, socket);
+        } else if (this.badRequest(req)) {
+            this.abort(400, socket);
+        } else if (this.versionInvalid(req)) {
+            this.abort(426, socket, ['Sec-Websocket-Version: 13']);
+        } else {
+            this.upgrade(req);
+        }
     }
 
-    routeValid(req: IncomingMessage): boolean {
+    upgrade(req: IncomingMessage): void {
+
+    }
+
+    abort(statusCode: number, socket: TLSSocket, headers?: string[]): void {
+        const res = formatHttpResponse(statusCode, headers);
+        socket.write(res, 'utf-8');
+        socket.end();
+    }
+
+    routeInvalid(req: IncomingMessage): boolean {
         const url = new URL(req.url, `https://${req.headers.host}`);
-        return this.#serviceRoute === url.pathname;
+        return this.#serviceRoute !== url.pathname;
     }
 
-    abortUpgrade(statusCode: number, socket: Duplex): void {
-        const res = formatHttpResponse(statusCode);
-        socket.end(res, 'utf-8');
+    versionInvalid(req: IncomingMessage): boolean {
+        return req.headers["sec-websocket-version"] !== '13';
+    }
+
+    originInvalid(req: IncomingMessage): boolean {
+        const reqOrigin = req.headers.origin;
+
+        if (reqOrigin && this.#origins) {
+            return !this.#origins.includes(reqOrigin);
+        }
+
+        // if an origin is required (i.e. client must be a browser), 
+        // return true (i.e. 'invalid') if no origin was found
+        if (this.#requireOrigin && !reqOrigin) {
+            return true;
+        } 
+            
+        // if no origins are specified, all origins are accepted
+        return false;  
+    }
+
+    badRequest(req: IncomingMessage): boolean {
+        if (Number(req.httpVersion) < 1.1) return true;
+
+        if (req.method !== 'GET') return true;
+
+        // uri?
+
+        //if (req.headers.host !== Server's Authority) {}
+
+        if (req.headers.upgrade !== 'websocket') return true;
+
+        if (req.headers.connection !== 'upgrade') return true;
+
+        return false;
     }
 
     // isWildcard(route: Pathname): boolean {
  
     // }
 
-}
-
-function formatHttpResponse(statusCode: number): string {
-    const status = http.STATUS_CODES[statusCode];
-    const formattedResponse = 
-    `HTTPS/1.1 ${statusCode} ${status}\r\n
-    \r\n`;
-
-    return formattedResponse;
 }
