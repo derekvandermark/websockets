4c8ac12b9fa0d931bd4f8960faaec82a565bab06
Derek van der Mark <derek.vandermark@gmail.com>
2024-06-19 17:16:03 -0400

    Add pathname to WebSocket class constructor

--- lib/websocket.ts
+++ lib/websocket.ts
@@ -1,17 +1,19 @@
-import { EventEmitter } from "stream";
+import EventEmitter from "events";
 import { TLSSocket } from "tls";
 import { STATE } from "./constants";
 import { ReadyState } from "./types";
 
 export default class WebSocket extends EventEmitter {
 
-    constructor(tlsSocket: TLSSocket) {
+    #tlsSocket: TLSSocket;
+    readyState: ReadyState;
+    // uri: the uri this websocket requested, so we know what purpose this websocket serves
+    // and can call the getter for all websockets connected which requested this URI initially
+    
+    constructor(tlsSocket: TLSSocket, pathname: string) {
         super();
-        this.tlsSocket = tlsSocket;
+        this.#tlsSocket = tlsSocket;
         this.readyState = STATE.CONNECTING;
     }
-
-    tlsSocket: TLSSocket;
-    readyState: ReadyState;
 
 } 
