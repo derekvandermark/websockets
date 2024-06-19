23e91dd7c9ab7288ea6d1b7cfd71c8e27720dceb
Derek van der Mark <derek.vandermark@gmail.com>
2024-06-19 17:23:27 -0400

    Update WSSOptions & add ConnectionList type

--- lib/types.ts
+++ lib/types.ts
@@ -1,4 +1,5 @@
 import { TLSSocket } from "tls";
+import WebSocket from "./websocket";
 
 export type ReadyState = 'CONNECTING' | 'OPEN' | 'CLOSING' | 'CLOSED';
 
@@ -12,7 +13,13 @@ export type ConnectionListener = (ws: TLSSocket) => void;
 export type Pathname = `/${string}`;
 
 export type WSSOptions = {
+    noServer?: boolean,
     origins?: string[],
     requireOrigin?: boolean,
     subProtocols?: string[]
 }
+
+/* an array of WebSockets, or in the case of a uri path with a wildcard as the last segment,
+ * an object with the keys being the wildcard segments with currently active connections,
+ * and the values being the array of active WebSockets at that specific route */
+export type ConnectionList = WebSocket[] | { [wildcardId: string]: WebSocket[] };;
